3c4ad9f9b1ee0bbd038c2f075de4c9de6285e194
Msg89 <mariasglynou@gmail.com>
2021-01-21 00:03:29 +0200

    Toggle questions and answers Vanilla JS

--- scripts.js
+++ scripts.js
@@ -1,12 +1,21 @@
+
+// ********************WORKING EXCEPT QUESTION STYLE 
 const q = document.querySelectorAll('.question');
+const a = document.querySelectorAll('.answer');
 
 q.forEach((q) => q.addEventListener('click', function () {
   if (this.nextElementSibling.className === 'open'){
-    this.nextElementSibling.className = 'close';
-    this.style.fontWeight = '';
-  } else {
-    this.nextElementSibling.className = 'open';
-    q.style.fontWeight =  '900';
-  }
-}));
+        this.nextElementSibling.className = 'close';
+        this.className = 'question';
+      
+      } else {
+        a.forEach((a) => { a.className = 'answer close';})
+        q.nextElementSibling.className = 'open';
+        q.forEach((q) => { q.className = 'question';})
+        q.className = 'question active';
+      };
+})); 
+ 
 
+
+ 
